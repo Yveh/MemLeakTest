e87279f9e2f1f9fd69c626864e064a7982e5621a
yveh <yveh1999@sjtu.edu.cn>
2019-11-25 01:18:51 -0800

    while(1)

--- code.cpp
+++ code.cpp
@@ -3,7 +3,7 @@ int main() {
     int *pp = new int [10];
   int a, b;
   scanf("%d%d", &a, &b);
-
+    while(1);
     fprintf(stderr, "output to stderr hahahahaha\n");
     printf("%d\n", a + b);
   
